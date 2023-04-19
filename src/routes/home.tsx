18d101818184858ae548016dba8c7b75058b2932
Praveen <111781869+pscode22@users.noreply.github.com>
2023-04-19 22:30:06 +0530

    style setup

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,6 +4,11 @@ export default function RouteHomePage() {
   return (
     <div>
       <h1 className="text-white">HomePage</h1>
+      <h2 className="text-white">HomePage</h2>
+      <h3 className="text-white">HomePage</h3>
+      <h4 className="text-white">HomePage</h4>
+      <h5 className="text-white">HomePage</h5>
+      <h6 className="text-white">HomePage</h6>
     </div>
   );
 }
