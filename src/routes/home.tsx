8eeea18b6edff9596849a412566e61e612c9d1b1
Praveen <111781869+pscode22@users.noreply.github.com>
2023-04-23 12:18:00 +0530

    dev branch

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RouteHomePage() {
   return (
     <div>
-      <h1 className="text-primary">HomePage</h1>
+      <h1 className="text-">HomePage</h1>
       <h2 className="text-white">HomePage</h2>
       <h3 className="text-white">HomePage</h3>
       <h4 className="text-white">HomePage</h4>
