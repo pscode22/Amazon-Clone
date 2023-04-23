fc9d0a19b3f3c5e6bc1ebd4e822d5e7cb333735e
Praveen <111781869+pscode22@users.noreply.github.com>
2023-04-23 12:12:51 +0530

    tailwind config setup

---
+++ public/tailwind-configuration/base-colors.ts
@@ -0,0 +1,24 @@
+export default function TailwindBaseColors() {
+  return {
+    primary: '#232f3e',
+    secondary: '#888c8c',
+    cyan: '#1196ab',
+    darkCyan: '#017285',
+    alert: '#ffaf38',
+    danger: '#cc1d3a',
+    yellow: '#FFD814',
+    vividOrange: '#FFA41C',
+
+    ...{
+      borderYellow: '#FCD200',
+      borderOrange: '#FF8F00'
+    },
+
+    ...{
+      textPrimary: '#0F1111',
+      textSecondary: '#565959',
+      textCyan: '#007185',
+      textHoverColor: '#C7511F'
+    }
+  };
+}
