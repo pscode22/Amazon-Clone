224b9f1a9c891a7b67163f2adb0de40975b0e745
Praveen <111781869+pscode22@users.noreply.github.com>
2023-04-24 21:42:35 +0530

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import amazonLogo from '@assets/png/amazon-logo.png';
 
 export default function MainHeader() {
   return (
-    <div className="w-full bg-[#131921] h-[6rem] px-2">
-      <div className="w-[13rem] h-full flex align-middle justify-center flex-col">
-        <img
-          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          alt="amazon-logo"
-          className="w-full h-[5rem]"
-        />
+    <div className="w-full bg-[#131921] h-[6rem] px-2  gap-1">
+      <div className="w-[12rem] h-full">
+        <div className="h-full flex align-middle justify-center py-2">
+          <img
+            src={amazonLogo}
+            alt="amazon-logo"
+            className="border border-transparent cursor-pointer hover:border-white p-2"
+          />
+        </div>
       </div>
+
+      <div className="my-[.5rem] border border-white">ded</div>
     </div>
   );
 }
